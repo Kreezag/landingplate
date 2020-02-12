6b359c0c4ea6afc96862dd88dc21e507a8268af9
Andrei Kuchuk <andrey.kuchuk@spiralscout.com>
2020-02-12 14:09:41 +0300

    update lib for landing workflow

--- webpack.config.js
+++ webpack.config.js
@@ -10,128 +10,57 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
 const isDev = process.env.NODE_ENV !== 'production';
 
-let envPlugins = [];
-if (isDev) {
-  envPlugins = [
-    new webpack.HotModuleReplacementPlugin(),
-  ];
-} else {
-  envPlugins = [
-    new MiniCssExtractPlugin({
-      filename: isDev ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
-    }),
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['dist']
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-  ];
-}
+// let envPlugins = [];
+// if (isDev) {
+//   envPlugins = [
+//     new webpack.HotModuleReplacementPlugin(),
+//   ];
+// } else {
+//   envPlugins = [
+//     new MiniCssExtractPlugin({
+//       filename: isDev ? '[name].css' : '[name].[hash].css',
+//       chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+//     }),
+//     new CleanWebpackPlugin({
+//       cleanAfterEveryBuildPatterns: ['dist']
+//     }),
+//     new webpack.HashedModuleIdsPlugin(),
+//   ];
+// }
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
   devServer: {
-    contentBase: './dist',
+    historyApiFallback: {index: '/'},
+    // contentBase: './dist',
     hot: true,
   },
   devtool: 'source-map',
   mode: isDev ? 'development' : 'production',
   module: {
     rules: [{
-        test: /\.scss$/,
-        use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'assets/'
-          }
-        }]
-      },
-      {
-        test: /\.(html)$/,
-        include: path.join(__dirname, 'src/partials'),
-        use: {
-          loader: 'html-loader',
-          options: {
-            minimize: !isDev,
-            interpolate: true,
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    ...envPlugins
-  ],
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        }
-      },
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
+      test: /\.less$/,
+      exclude: /(node_modules)/,
+      use: ['style-loader', 'css-loader?-url&sourceMap', 'less-loader?sourceMap'],
     },
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-      }),
-      new OptimizeCSSAssetsPlugin({})
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        exclude: /node_modules/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+
+      },
     ]
   },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
-  }
 }
