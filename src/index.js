4e10bd082f6e01509063179443175146a14daef6
Andrei Kuchuk <andrey.kuchuk@spiralscout.com>
2020-02-17 19:00:22 +0300

    update mobile menu in app page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './less/index.less'
 
-const menuOpenerEl = window.document.getElementById('mobile-menu-opener');
+const menuOpenerEl = window.document.getElementById('page-mobile-menu-opener');
+const bodyElement = window.document.querySelector('body');
 
-menuOpenerEl.onclick = ((e) => {
-  menuOpenerEl.classList.toggle('mobile-menu-opener--open');
+menuOpenerEl.onclick = (() => {
+  bodyElement.classList.toggle('page-mobile-menu-open');
 })
+
+const menuElements = window.document.getElementsByClassName('main__menu-item-link')
+
+for (let i = 0; i < menuElements.length; i ++) {
+  menuElements[i].onclick = () => {
+    bodyElement.classList.remove('page-mobile-menu-open');
+  }
+}
+
+window.document.getElementById('page-mobile-menu-button-request').onclick = () => {
+  bodyElement.classList.remove('page-mobile-menu-open');
+}
