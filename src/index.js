77c28a3d53f4ac62e65fef1f2826cf11aa157ede
Andrei Kuchuk <andrey.kuchuk@spiralscout.com>
2020-02-18 11:17:52 +0300

    create mobile-nav as separate module

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import './less/index.less'
 
-const menuOpenerEl = window.document.getElementById('page-mobile-menu-opener');
+const menuOpenerEl = window.document.getElementById('mobile-menu-opener');
+const menuCloseEl = window.document.getElementById('mobile-menu-closer');
+
 const bodyElement = window.document.querySelector('body');
 
 menuOpenerEl.onclick = (() => {
-  bodyElement.classList.toggle('page-mobile-menu-open');
+  bodyElement.classList.toggle('mobile-menu-open');
 })
+
+menuCloseEl.onclick = () => {
+  bodyElement.classList.toggle('mobile-menu-open');
+}
 
 const menuElements = window.document.getElementsByClassName('main__menu-item-link')
 
 for (let i = 0; i < menuElements.length; i ++) {
   menuElements[i].onclick = () => {
-    bodyElement.classList.remove('page-mobile-menu-open');
+    bodyElement.classList.remove('mobile-menu-open');
   }
 }
 
 window.document.getElementById('page-mobile-menu-button-request').onclick = () => {
-  bodyElement.classList.remove('page-mobile-menu-open');
+  bodyElement.classList.remove('mobile-menu-open');
 }
